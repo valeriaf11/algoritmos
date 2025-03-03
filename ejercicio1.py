6ad8c039d9ce4adb355c8b30f905ae96b8f27872
valeriaf11 <salinastejadaemmavaleria@gmail.com>
2025-03-03 11:14:36 -0600

    ejercicio 2 en CodeSandbox
    definimos valores de variables y las imprimimos sobre algún hobby, usamos: enteros, booleanos y caracteres.

---
+++ ejercicio1.py
@@ -0,0 +1,28 @@
+#Numero se asigna un valor a una variable
+numero= 10
+
+#Se imprime el valor de la variable numero, ej. 10
+print("El numero asignado es:", numero)
+
+"""
+Actividad de clase: Van a definir valores de variables y las van a imprimir; sobre algun hobby
+Usaras:
+-Enteros
+-Booleanos 
+-Caracteres
+Tiempo: 8 min
+"""
+# Definición de variables
+# Entero: Número de meses entrenando boxeo
+meses_entrenando = 10  
+
+# Booleano: Indica si el boxeo es su hobby favorito
+es_hobby_favorito = True  
+
+# Caracter: Nivel de dificultad que percibe (A: Alta, M: Media, B: Baja)
+nivel_dificultad = 'M'  
+
+# Impresión de valores
+print("Tiempo entrenando boxeo:", meses_entrenando, "meses")
+print("¿Es el boxeo mi hobby favorito?", es_hobby_favorito)
+print("Nivel de dificultad percibido:", nivel_dificultad)
